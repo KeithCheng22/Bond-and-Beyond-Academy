4307c6eba264e8f463e4d6a3095a504e4c34d4c7
Keith <keithcheng.sgs@gmail.com>
2024-08-14 14:58:41 +0800

    minor styling fix to navbar

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
           ></path>
         </svg>
         <Services />
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
           <path
             fill="#ebe0d7"
             fillOpacity="1"
