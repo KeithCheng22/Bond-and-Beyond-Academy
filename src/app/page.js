34651c5b4e6a626eed3c961bbcbea38788343d50
Keith <keithcheng.sgs@gmail.com>
2024-08-14 15:05:39 +0800

    fix svg line

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
           ></path>
         </svg>
         <Services />
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 310">
           <path
             fill="#ebe0d7"
             fillOpacity="1"
